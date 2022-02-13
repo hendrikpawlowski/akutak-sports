03a0103a6edc86da9e58874baf5c5b8741d5bbef
Hendrik Pawlowski <hendrik@Air-von-Hendrik.fritz.box>
2022-02-13 10:56:06 +0100

    animations

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,5 @@
-function getScrollPercent() {
-    var h = document.documentElement,
-        b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-}
-
 const init = function () {
-    window.addEventListener("scroll", () => {
-        console.log(getScrollPercent());
-    })
+    AOS.init();
 }
 
 init();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    fontFamily: {
+      sans: ['"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial']
+    },
     extend: {},
   },
   plugins: [],
